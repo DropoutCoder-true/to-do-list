3cb2326f59600a3ccab2ceb9cd73f46b08789042
vighnesh <vighneshzagade4@gmail.com>
2024-11-23 12:52:51 +0530

    implemented-raw-orientation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,44 @@
+import { Box, Container, Typography } from "@mui/material";
+
 function App() {
-  return <h1>Hello, from this page!</h1>;
+  return (
+    <Container
+      maxWidth={false}
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        width: "100vw",
+        backgroundColor: "pink",
+        padding: "4rem 3rem",
+      }}
+    >
+      <Box
+        sx={{
+          backgroundColor: "white",
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          padding: "3rem",
+        }}
+      >
+        <Typography
+          variant="h3"
+          sx={{
+            backgroundColor: "#F6D6D6",
+            color: "black",
+            width: "100%",
+            alignContent: "center",
+          }}
+        >
+          To Do List
+        </Typography>
+      </Box>
+    </Container>
+  );
 }
 
 export default App;
